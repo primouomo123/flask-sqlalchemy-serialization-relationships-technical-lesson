1216aad44a3c315437c9d73c1818979c63296170
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-02-15 16:40:10 -0500

    final solution

--- server/models.py
+++ server/models.py
@@ -30,6 +30,13 @@ class Animal(db.Model):
     def __repr__(self):
         return f'<Animal {self.name}, a {self.species}>'
 
+# Create AnimalSchema, inheritting from Schema
+class AnimalSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String()
+    species = fields.String()
+    zookeeper = fields.Nested(lambda: ZookeeperSchema(exclude=("animals",)))
+    enclosure = fields.Nested(lambda: EnclosureSchema(exclude=("animals",)))
 
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
@@ -40,6 +47,12 @@ class Zookeeper(db.Model):
 
     animals = db.relationship('Animal', back_populates='zookeeper')
 
+# Create ZookeeperSchema, inheritting from Schema
+class ZookeeperSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String()
+    birthday = fields.DateTime()
+    animals = fields.List(fields.Nested(AnimalSchema(exclude=("zookeeper",))))
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
@@ -49,3 +62,10 @@ class Enclosure(db.Model):
     open_to_visitors = db.Column(db.Boolean)
 
     animals = db.relationship('Animal', back_populates='enclosure')
+
+# Create EnclosureSchema, inheritting from Schema
+class EnclosureSchema(Schema):
+    id = fields.Int(dump_only=True)
+    environment = fields.String()
+    open_to_visitors = fields.Boolean()
+    animals = fields.List(fields.Nested(AnimalSchema(exclude=("enclosure",))))
